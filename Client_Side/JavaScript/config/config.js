f5a0066c34a9adcb192444c7b2dcfad9056081fb
aparnaprem <ammuparu2@gmail.com>
2017-03-07 11:20:54 +0530

    Update config.js

--- Client_Side/JavaScript/config/config.js
+++ Client_Side/JavaScript/config/config.js
@@ -64,87 +64,87 @@ config.participants.scrap_merchant = {};
 
 // Regulators
 config.participants.users.regulators[0]= {};
-config.participants.users.regulators[0].company = 'DVLA';
+config.participants.users.regulators[0].company = 'Kollur';
 config.participants.users.regulators[0].type = 'Regulator';
 config.participants.users.regulators[0].user = 'Ronald';
 
 // Manufacturers
 config.participants.users.manufacturers[0] = {};
-config.participants.users.manufacturers[0].company = 'Alfa Romeo';
+config.participants.users.manufacturers[0].company = 'Shah';
 config.participants.users.manufacturers[0].type = 'Manufacturer';
 config.participants.users.manufacturers[0].user = 'Martin';
 
 config.participants.users.manufacturers[1] = {};
-config.participants.users.manufacturers[1].company = 'Toyota';
+config.participants.users.manufacturers[1].company = 'Kothari';
 config.participants.users.manufacturers[1].type = 'Manufacturer';
 config.participants.users.manufacturers[1].user = 'Maria';
 
 config.participants.users.manufacturers[2] = {};
-config.participants.users.manufacturers[2].company = 'Jaguar Land Rover';
+config.participants.users.manufacturers[2].company = 'Agarwal';
 config.participants.users.manufacturers[2].type = 'Manufacturer';
 config.participants.users.manufacturers[2].user = 'Mandy';
 
 // Dealerships
 config.participants.users.dealerships[0] = {};
-config.participants.users.dealerships[0].company = 'Beechvale Group';
+config.participants.users.dealerships[0].company = 'Ajay_Gosh';
 config.participants.users.dealerships[0].type = 'Dealership';
 config.participants.users.dealerships[0].user = 'Deborah';
 
 config.participants.users.dealerships[1] = {};
-config.participants.users.dealerships[1].company = 'Milescape';
+config.participants.users.dealerships[1].company = 'Andrew_Hurt';
 config.participants.users.dealerships[1].type = 'Dealership';
 config.participants.users.dealerships[1].user = 'Dennis';
 
 config.participants.users.dealerships[2] = {};
-config.participants.users.dealerships[2].company = 'Viewers Alfa Romeo';
+config.participants.users.dealerships[2].company = 'Rahul_Ajay_Gandhi';
 config.participants.users.dealerships[2].type = 'Dealership';
 config.participants.users.dealerships[2].user = 'Delia';
 
 // Lease Companies
 config.participants.users.lease_companies[0] = {};
-config.participants.users.lease_companies[0].company = 'LeaseCan';
+config.participants.users.lease_companies[0].company = 'Crayon_Bros_Ltd';
 config.participants.users.lease_companies[0].type = 'Lease Company';
 config.participants.users.lease_companies[0].user = 'Lesley';
 
 config.participants.users.lease_companies[1] = {};
-config.participants.users.lease_companies[1].company = 'Every Car Leasing';
+config.participants.users.lease_companies[1].company = 'Aston_Cutting_Centre';
 config.participants.users.lease_companies[1].type = 'Lease Company';
 config.participants.users.lease_companies[1].user = 'Larry';
 
 config.participants.users.lease_companies[2] = {};
-config.participants.users.lease_companies[2].company = 'Regionwide Vehicle Contracts';
+config.participants.users.lease_companies[2].company = 'ScrapIt';
 config.participants.users.lease_companies[2].type = 'Lease Company';
 config.participants.users.lease_companies[2].user = 'Luke';
 
 // Leasees
 config.participants.users.leasees[0] = {};
-config.participants.users.leasees[0].company = 'Joe Payne';
+config.participants.users.leasees[0].company = 'Adora';
 config.participants.users.leasees[0].type = 'Leasee';
 config.participants.users.leasees[0].user = 'Joe';
 
 config.participants.users.leasees[1] = {};
-config.participants.users.leasees[1].company = 'Andrew Hurt';
+config.participants.users.leasees[1].company = 'Tanishq';
 config.participants.users.leasees[1].type = 'Leasee';
 config.participants.users.leasees[1].user = 'Andrew';
 
 config.participants.users.leasees[2] = {};
-config.participants.users.leasees[2].company = 'Anthony O\'Dowd';
+config.participants.users.leasees[2].company = 'Kiah';
 config.participants.users.leasees[2].type = 'Leasee';
 config.participants.users.leasees[2].user = 'Anthony';
 
 // Scrap Merchants
 config.participants.users.scrap_merchants[0] = {};
-config.participants.users.scrap_merchants[0].company = 'Cray Bros (London) Ltd';
+config.participants.users.scrap_merchants[0].company = 'Gaurav_Singh';
 config.participants.users.scrap_merchants[0].type = 'Scrap Merchant';
 config.participants.users.scrap_merchants[0].user = 'Sandy';
 
 config.participants.users.scrap_merchants[1] = {};
-config.participants.users.scrap_merchants[1].company = 'Aston Scrap Centre';
+config.participants.users.scrap_merchants[1].company = 'Adwaith';
 config.participants.users.scrap_merchants[1].type = 'Scrap Merchant';
 config.participants.users.scrap_merchants[1].user = 'Scott';
 
 config.participants.users.scrap_merchants[2] = {};
-config.participants.users.scrap_merchants[2].company = 'ScrapIt! UK';
+config.participants.users.scrap_merchants[2].company = 'AmarDev';
 config.participants.users.scrap_merchants[2].type = 'Scrap Merchant';
 config.participants.users.scrap_merchants[2].user = 'Sid';
 
