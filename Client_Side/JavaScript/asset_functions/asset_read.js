429339ea6a2c7c71b8279ec5a97c2831d008301c
aparnaprem <ammuparu2@gmail.com>
2017-03-19 00:57:39 +0530

    Update asset_read.js

--- Client_Side/JavaScript/asset_functions/asset_read.js
+++ Client_Side/JavaScript/asset_functions/asset_read.js
@@ -36,17 +36,21 @@ function loadAssets()
 					{
 						if(obj.status == 0)
 						{
-							obj.VIN = '&lt;<i>VIN</i>&gt;';
-							obj.make = '&lt;<i>make</i>&gt;';
-							obj.model = '&lt;<i>model</i>&gt;';
+							obj.clarity = '&lt;<i>clarity</i>&gt;';
+							obj.diamondat = '&lt;<i>diamondat</i>&gt;';
+							obj.cut = '&lt;<i>cut</i>&gt;';
 							obj.colour = '&lt;<i>colour</i>&gt;';
-							obj.reg = '&lt;<i>registration</i>&gt;';
+							obj.date = '&lt;<i>date</i>&gt;';	
+							obj.symmetry = '&lt;<i>symmetry</i>&gt;';
+							obj.polish = '&lt;<i>polish</i>&gt;';
+							obj.timestamp = '&lt;<i>timestamp</i>&gt;';		
+							obj.jewellerytype = '&lt;<i>jewellerytype</i>&gt;';
 							objects.push(obj);
 						}
 					}
 					else
 					{
-						if(typeof obj.message == 'undefined' && obj.VIN > 0 && obj.make.toLowerCase() != 'undefined' && obj.make.trim() != '' && obj.model.toLowerCase() != 'undefined' && obj.model.trim() != '' && obj.reg.toLowerCase() != 'undefined' && obj.reg.trim() != '' && obj.colour.toLowerCase() != 'undefined' && obj.colour.trim() != '' && !obj.scrapped)
+						if(typeof obj.message == 'undefined' && obj.clarity > 0 && obj.diamondat.toLowerCase() != 'undefined' && obj.diamondat.trim() != '' && obj.cut.toLowerCase() != 'undefined' && obj.cut.trim() != '' && obj.Date.toLowerCase() != 'undefined' && obj.Date.trim() != '' && obj.colour.toLowerCase() != 'undefined' && obj.colour.trim() != '' && !obj.scrapped)
 						{
 							objects.push(obj)
 						}
@@ -75,7 +79,7 @@ function loadAssets()
 				for(var i = 0; i < objects.length; i++)
 				{
 					var data = objects[i];
-					$("#vhclsTbl").append("<tr class='vehRw'><td class='vin'>"+data.VIN+"</td><td class='vehDets' ><span class='carInfo'>" + data.make + "</span><span class='carInfo'>" + data.model + ", </span><span class='carInfo'>" + data.colour + ", </span><span class='carInfo'>" + data.reg + "</span></td><td class='chkHldr'><span class='chkSpc' ></span><span class='chkBx' ></span><input class='isChk' type='hidden' value='false' /><input class='v5cID' type='hidden' value='"+data.v5cID+"' /></td></tr>");
+					$("#vhclsTbl").append("<tr class='assetRw'><td class='clarity'>"+data.clarity+"</td><td class='assetDets' ><span class='diamondInfo'>" + data.diamondat + "</span><span class='diamondInfo'>" + data.cut + ", </span><span class='diamondInfo'>" + data.colour + ", </span><span class='diamondInfo'>" + data.date + "</span></td><td class='chkHldr'><span class='chkSpc' ></span><span class='chkBx' ></span><input class='isChk' type='hidden' value='false' /><input class='assetID' type='hidden' value='"+data.assetID+"' /></td></tr>");
 				}
 				changeBarSize();
 			}
@@ -139,12 +143,12 @@ function loadUpdateAssets()
 			for(var i = 0; i < d.length; i++)
 			{
 				var data = d[i];
-				if(data.VIN == 0) data.VIN = '&lt;<i>VIN</i>&gt;';
-				if(data.make.toLowerCase() == 'undefined' || data.make.trim() == '') data.make = '&lt;<i>make</i>&gt;';
-				if(data.model.toLowerCase() == 'undefined' || data.model.trim() == '') data.model = '&lt;<i>model</i>&gt;';
-				if(data.reg.toLowerCase() == 'undefined' || data.reg.trim() == '') data.reg = '&lt;<i>registration</i>&gt;';
+				if(data.clarity == 0) data.clarity = '&lt;<i>clarity</i>&gt;';
+				if(data.diamondat.toLowerCase() == 'undefined' || data.diamondat.trim() == '') data.diamondat = '&lt;<i>diamondat</i>&gt;';
+				if(data.cut.toLowerCase() == 'undefined' || data.cut.trim() == '') data.cut = '&lt;<i>cut</i>&gt;';
+				if(data.date.toLowerCase() == 'undefined' || data.date.trim() == '') data.date = '&lt;<i>date</i>&gt;';
 				if(data.colour.toLowerCase() == 'undefined' || data.colour.trim() == '') data.colour = '&lt;<i>colour</i>&gt;';
-				$('<tr class="foundCars" ><td class="smlBrk"></td><td class="editRw" ><span class="carID">'+data.v5cID+'</span></td><td class="editRw" colspan="2" >[<span class="carVin">'+data.VIN+'</span>] <span class="carMake">'+data.make+'</span> <span class="carModel">'+data.model+'</span>, <span class="carColour">'+data.colour+'</span>, <span class="carReg">'+data.reg+'</span><img src="Icons/Manufacturer/edit.svg" onclick="showEditTbl(this)" class="rtBtn" width="20" height="20" /></td><td class="smlBrk" ></td></tr>').insertAfter('#insAft');
+				$('<tr class="founddiamonds" ><td class="smlBrk"></td><td class="editRw" ><span class="diamondID">'+data.assetID+'</span></td><td class="editRw" colspan="2" >[<span class="diamondclarity">'+data.clarity+'</span>] <span class="diamonddiamondat">'+data.diamondat+'</span> <span class="diamondcut">'+data.cut+'</span>, <span class="diamondColour">'+data.colour+'</span>, <span class="diamondDate">'+data.date+'</span><span class="diamondtimestamp">'+data.Timestamp+'</span><span class="diamondSymmetry">'+data.Symmetry+'</span><span class="diamondPolish">'+data.Polish+'</span><span class="diamondjewellerytype">'+data.jewellerytype+'</span><img src="Icons/distributor/edit.svg" onclick="showEditTbl(this)" class="rtBtn" width="20" height="20" /></td><td class="smlBrk" ></td></tr>').insertAfter('#insAft');
 			}
 		}
 	}
